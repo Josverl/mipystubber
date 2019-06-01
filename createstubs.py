a2d983a4ca94ff38323b91fc6f375f9cbdc27160
Jos Verlinde <jos_verlinde@hotmail.com>
2019-06-01 16:14:59 +0200

    reduced logging to support esp8622 1.9.4

--- createstubs.py
+++ createstubs.py
@@ -4,13 +4,13 @@ Copyright (c) 2019 Jos Verlinde
 """
 import errno
 import gc
-import logging
+# import logging
 import os
 import sys
 from time import sleep_us
 from ujson import dumps
-
-stubber_version = '1.1.0'
+#pylint: disable=bare-except
+_version = '1.1.0'
 # deal with firmware specific implementations
 try:
     from machine import resetWDT
@@ -21,27 +21,29 @@ except:
 class Stubber():
     "Generate stubs for modules in firmware"
     def __init__(self, path: str = None):
-        self._log = logging.getLogger('stubber')
+        #self._log = logging.getLogger('stubber')
         self._report = []
         u = os.uname()
         self._report.append({'sysname': u.sysname, 'nodename': u.nodename, 'release': u.release, 'version': u.version, 'machine': u.machine})
-        self._report.append({'stubber': stubber_version})
+        del u
+        self._report.append({'stubber': _version})
         if path:
             #get rid of trailing slash
             if path.endswith('/'):
-                path = path[:-1]
+                self.path = path[:-1]
+            else:
+                self.path = path
         else:
             #determine path for stubs
-            path = "{}/stubs/{}".format(
-                self.get_root(),
-                self.firmware_ID(asfile=True)
+            self.path = "{}/stubs/{}".format(
+                get_root(),
+                firmware_ID(asfile=True)
                 ).replace('//', '/')
-
-        self.path = path
         try:
-            self.ensure_folder(path + "/")
+            ensure_folder(path + "/")
         except:
-            self._log.error("error creating stub folder {}".format(path))
+            pass
+            #self._log.error("error creating stub folder {}".format(path))
         self.problematic = ["upysh", "webrepl_setup", "http_client", "http_client_ssl", "http_server", "http_server_ssl"]
         self.excluded = ["webrepl", "_webrepl", "webrepl_setup"]
         # there is no option to discover modules from upython, need to hardcode
@@ -60,31 +62,9 @@ class Stubber():
         #try to avoid running out of memory with nested mods
         self.include_nested = gc.mem_free() > 3200
 
-
-
-    def get_obj_attributes(self, obj: object):
-        "extract information of the objects members and attributes"
-        result = []
-        errors = []
-        #self._log.info('get attributes {} {}'.format(repr(obj),obj ))
-        for name in dir(obj):
-            try:
-                val = getattr(obj, name)
-                # name , value , type
-                result.append((name, repr(val), repr(type(val)), val))
-                #self._log.info( result[-1])
-            except BaseException as e:
-                errors.append("Couldn't get attribute '{}' from object '{}', Err: {}".format(name, obj, e))
-        gc.collect()
-        return result, errors
-
-    def add_modules(self, modules: list):
-        "Add additional modules to be exported"
-        self.modules = sorted(set(self.modules) | set(modules))
-
     def create_all_stubs(self):
         "Create stubs for all configured modules"
-        self._log.info("Start micropython-stubber v{} on {}".format(stubber_version, self.firmware_ID()))
+        #self._log.info("Start micropython-stubber v{} on {}".format(_version, firmware_ID()))
         # start with the (more complex) modules with a / first to reduce memory problems
         self.modules = [m for m in self.modules if '/' in m] + [m for m in self.modules if '/' not in m]
         gc.collect()
@@ -94,13 +74,13 @@ class Stubber():
                 self.include_nested = gc.mem_free() > 3200
             
             if module_name.startswith("_") and module_name != '_thread':
-                self._log.warning("Skip module: {:<20}        : internal ".format(module_name))
+                #self._log.warning("Skip module: {:<20}        : internal ".format(module_name))
                 continue
             if module_name in self.problematic:
-                self._log.warning("Skip module: {:<20}        : Known problematic".format(module_name))
+                #self._log.warning("Skip module: {:<20}        : Known problematic".format(module_name))
                 continue
             if module_name in self.excluded:
-                self._log.warning("Skip module: {:<20}        : Excluded".format(module_name))
+                #self._log.warning("Skip module: {:<20}        : Excluded".format(module_name))
                 continue
 
             file_name = "{}/{}.py".format(
@@ -109,50 +89,50 @@ class Stubber():
             )
             gc.collect()
             m1 = gc.mem_free()
-            self._log.info("Stub module: {:<20} to file: {:<55} mem:{:>5}".format(module_name, file_name, m1))
+            print("Stub module: {:<20} to file: {:<55} mem:{:>5}".format(module_name, file_name, m1))
+            #self._log.info("Stub module: {:<20} to file: {:<55} mem:{:>5}".format(module_name, file_name, m1))
             try:
                 self.create_module_stub(module_name, file_name)
             except:
                 pass
             gc.collect()
-            m2 = gc.mem_free()
-            self._log.debug("Memory     : {:>20} {:>6}".format(m1, m1-m2))
-        self._log.info('Finally done')
+            # m2 = gc.mem_free()
+            #self._log.debug("Memory     : {:>20} {:>6}".format(m1, m1-m2))
+        #self._log.info('Finally done')
 
     def create_module_stub(self, module_name: str, file_name: str = None):
         "Create a Stub of a single python module"
         if module_name.startswith("_") and module_name != '_thread':
-            self._log.warning("SKIPPING internal module:{}".format(module_name))
+            #self._log.warning("SKIPPING internal module:{}".format(module_name))
             return
 
         if module_name in self.problematic:
-            self._log.warning("SKIPPING problematic module:{}".format(module_name))
+            #self._log.warning("SKIPPING problematic module:{}".format(module_name))
             return
+        # if file_name is None: 
+        #     file_name = module_name.replace('.', '/') + ".py"
+
         if '/' in module_name:
             #for nested modules
-            self.ensure_folder(file_name)
+            ensure_folder(path=file_name)
             module_name = module_name.replace('/', '.')
             if not self.include_nested:
-                self._log.warning("SKIPPING nested module:{}".format(module_name))
+                #self._log.warning("SKIPPING nested module:{}".format(module_name))
                 return
-
-        if file_name is None:
-            file_name = module_name.replace('.', '_') + ".py"
 
         #import the module (as new_module) to examine it
         try:
             new_module = __import__(module_name)
         except ImportError as e:
-            self._log.debug("Unable to import module: {} : {}".format(module_name, e))
+            #self._log.debug("Unable to import module: {} : {}".format(module_name, e))
             return
         except e:
-            self._log.error("Failed to import module: {}".format(module_name))
-
+            #self._log.error("Failed to import module: {}".format(module_name))
             return
 
         # Start a new file
         with open(file_name, "w") as fp:
-            s = "\"\"\"\nModule: '{0}' on {1}\n\"\"\"\n# MCU: {2}\n# Stubber: {3}\n".format(module_name, self.firmware_ID(), os.uname(), stubber_version)
+            s = "\"\"\"\nModule: '{0}' on {1}\n\"\"\"\n# MCU: {2}\n# Stubber: {3}\n".format(module_name, firmware_ID(), os.uname(), _version)
             fp.write(s)
             self.write_object_stub(fp, new_module, module_name, "")
             self._report.append({"module":module_name, "file": file_name})
@@ -162,23 +142,25 @@ class Stubber():
             try:
                 del new_module
             except BaseException:
-                self._log.warning("could not del new_module")
+                pass
+                #self._log.warning("could not del new_module")
             try:
                 del sys.modules[module_name]
             except BaseException:
-                self._log.debug("could not del modules[{}]".format(module_name))
+                pass
+                #self._log.debug("could not del modules[{}]".format(module_name))
             gc.collect()
 
     def write_object_stub(self, fp, object_expr: object, obj_name: str, indent: str):
         "Write an object stub to an open file. Can call recursive."
         if object_expr in self.problematic:
-            self._log.warning("SKIPPING problematic module:{}".format(object_expr))
+            #self._log.warning("SKIPPING problematic module:{}".format(object_expr))
             return
 
-        self._log.debug("DUMPING : {}".format(object_expr))
-        items, errors = self.get_obj_attributes(object_expr)
-        if errors:
-            self._log.error(errors)
+        #self._log.debug("DUMPING : {}".format(object_expr))
+        items, errors = get_obj_attributes(object_expr)
+        # if errors:
+            #self._log.error(errors)
 
         for name, rep, typ, obj in sorted(items, key=lambda x: x[0]):
             if name.startswith("__"):
@@ -188,18 +170,18 @@ class Stubber():
             resetWDT()
             sleep_us(1)
 
-            self._log.debug("DUMPING {}{}{}:{}".format(indent, object_expr, name, typ))
+            #self._log.debug("DUMPING {}{}{}:{}".format(indent, object_expr, name, typ))
 
             if typ in ["<class 'function'>", "<class 'bound_method'>"]:
                 s = indent + "def " + name + "():\n"
                 s += indent + "    pass\n\n"
                 fp.write(s)
-                self._log.debug(s)
+                #self._log.debug(s)
 
             elif typ in ["<class 'str'>", "<class 'int'>", "<class 'float'>"]:
                 s = indent + name + " = " + rep + "\n"
                 fp.write(s)
-                self._log.debug(s)
+                #self._log.debug(s)
             #new class
             elif typ == "<class 'type'>" and indent == "":
                 # full expansion only on toplevel
@@ -208,9 +190,9 @@ class Stubber():
                 s += indent + "    ''\n"
 
                 fp.write(s)
-                self._log.debug(s)
+                #self._log.debug(s)
 
-                self._log.debug("#recursion !!")
+                #self._log.debug("#recursion !!")
                 self.write_object_stub(fp, obj, "{0}.{1}".format(obj_name, name), indent + "    ")
             else:
                 # keep only the name
@@ -222,37 +204,25 @@ class Stubber():
         except:
             pass
 
-    @staticmethod
-    def firmware_ID(asfile: bool = False):
-        "Get a sensible firmware ID"
-        if os.uname().sysname in 'esp32_LoBo':
-            #version in release
-            ver = os.uname().release
-        else:
-            # version before '-' in version
-            ver = os.uname().version.split('-')[0]
-        fid = "{} {}".format(os.uname().sysname, ver)
-        if asfile:
-            # path name restrictions
-            chars = " .()/\\:$"
-            for c in chars:
-                fid = fid.replace(c, "_")
-        return fid
 
     def clean(self):
         "Remove all files from the stub folder"
-        self._log.info("Clean/remove files in stubfolder: {}".format(self.path))
-        for fn in os.listdir(self.path):
-            try:
-                os.remove("{}/{}".format(self.path, fn))
-            except:
-                pass
+        #self._log.info("Clean/remove files in stubfolder: {}".format(self.path))
+        print("Clean/remove files in stubfolder: {}".format(self.path))
+        try:
+            for fn in os.listdir(self.path):
+                try:
+                    os.remove("{}/{}".format(self.path, fn))
+                except:
+                    pass
+        except:
+            pass
 
     def report(self, filename: str = "modules.json"):
         "create json with list of exported modules"
-        self._log.info("Created stubs for {} modules on board {}\nPath: {}".format(
+        print("Created stubs for {} modules on board {}\nOutput Path: {}".format(
             len(self._report)-2,
-            self.firmware_ID(),
+            firmware_ID(),
             self.path
             ))
         f_name = "{}/{}".format(self.path, filename)
@@ -270,59 +240,103 @@ class Stubber():
                     f.write(dumps(n))
                 f.write(']')
         except:
-            self._log.error("Failed to create the report.")
+            pass
+            #self._log.error("Failed to create the report.")
 
-    def ensure_folder(self, path: str):
-        "Create nested folders if needed"
-        i = start = 0
-        while i != -1:
-            i = path.find('/', start)
-            if i != -1:
-                if i == 0:
-                    p = path[0]
-                else:
-                    p = path[0:i]
-                # p = partial folder
-                try:
-                    _ = os.stat(p)
-                except OSError as e:
-                    # folder does not exist
-                    if e.args[0] == errno.ENOENT:
-                        try:
-                            os.mkdir(p)
-                        except OSError as e2:
-                            self._log.error('failed to create folder {}'.format(p))
-                            raise e2
-                    else:
-                        self._log.error('failed to create folder {}'.format(p))
-                        raise e
-            #next level deep
-            start = i+1
-
-    @staticmethod
-    def get_root():
-        "Determine the root folder of the device"
-        try:
-            r = "/flash"
-            _ = os.stat(r)
-        except OSError as e:
-            if e.args[0] == errno.ENOENT:
-                r = os.getcwd()
+def ensure_folder(path: str):
+    "Create nested folders if needed"
+    i = start = 0
+    while i != -1:
+        i = path.find('/', start)
+        if i != -1:
+            if i == 0:
+                p = path[0]
             else:
-                r = '/'
-        return r
+                p = path[0:i]
+            # p = partial folder
+            try:
+                _ = os.stat(p)
+            except OSError as e:
+                # folder does not exist
+                if e.args[0] == errno.ENOENT:
+                    try:
+                        os.mkdir(p)
+                    except OSError as e2:
+                        print('failed to create folder {}'.format(p))
+                        raise e2
+                else:
+                    print('failed to create folder {}'.format(p))
+                    ##self._log.error('failed to create folder {}'.format(p))
+                    raise e
+        #next level deep
+        start = i+1
+
+    # def add_modules(self, modules: list):
+    #     "Add additional modules to be exported"
+    #     self.modules = sorted(set(self.modules) | set(modules))
+
+
+def firmware_ID(asfile: bool = False):
+    "Get a sensible firmware ID"
+    if os.uname().sysname in 'esp32_LoBo':
+        #version in release
+        ver = os.uname().release
+    else:
+        # version before '-' in version
+        ver = os.uname().version.split('-')[0]
+    fid = "{} {}".format(os.uname().sysname, ver)
+    if asfile:
+        # path name restrictions
+        chars = " .()/\\:$"
+        for c in chars:
+            fid = fid.replace(c, "_")
+    return fid
+
+
+def get_root():
+    "Determine the root folder of the device"
+    try:
+        r = "/flash"
+        _ = os.stat(r)
+    except OSError as e:
+        if e.args[0] == errno.ENOENT:
+            r = os.getcwd()
+        else:
+            r = '/'
+    return r
+
+def get_obj_attributes(obj: object):
+    "extract information of the objects members and attributes"
+    result = []
+    errors = []
+    ##self._log.info('get attributes {} {}'.format(repr(obj),obj ))
+    for name in dir(obj):
+        try:
+            val = getattr(obj, name)
+            # name , value , type
+            result.append((name, repr(val), repr(type(val)), val))
+            ##self._log.info( result[-1])
+        except BaseException as e:
+            errors.append("Couldn't get attribute '{}' from object '{}', Err: {}".format(name, obj, e))
+    gc.collect()
+    return result, errors
+
 
 
 def main():
-    try:
-        logging.basicConfig(level=logging.INFO)
-    except:
-        pass
+    global stubber
+    # try:
+    #     logging.basicConfig(level=logging.INFO)
+    # except:
+    #     pass
     # Now clean up and get to work
+    print("start")
     stubber = Stubber()
     #stubber.add_modules(['xyz'])
-    stubber.clean()
+    #stubber.clean()
+    print("create")
     stubber.create_all_stubs()
+    print("report")
     stubber.report()
-
+    print("done")
 main()
