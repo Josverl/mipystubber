6eace7523e85f834d567a6a9b06e0196f5521fab
Jos Verlinde <jos_verlinde@hotmail.com>
2020-01-02 23:22:29 +0100

    [stubs] add MicroPython compatibility modules

---
+++ stubs/cpython-core/utarfile.pyi
@@ -0,0 +1,6 @@
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
+def TarFile(fileobj: Any) -> Any: ...
+    #   0: return tarfile.TarFile.open(fileobj=fileobj,mode=)
+    # ? 0: return tarfile.TarFile.open(fileobj=fileobj, mode=)
